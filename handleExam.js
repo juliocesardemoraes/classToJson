1aa3825882313bbfd110d1e2819199b56f66c363
julio cesar <jcmcf2012@gmail.com>
2023-01-28 17:20:20 -0300

    feat: script for creating classes

---
+++ handleExam.js
@@ -0,0 +1,281 @@
+let exam = `
+1- O condutor de veículo que se preocupa em avisar o outro que está rodando com o pneu murcho ou com a porta semifechada está:
+a) Cumprindo a legislação do trânsito
+b) Prejudicando a sua atenção no trânsito, podendo causar um acidente
+c) Cometendo uma infração
+d) Dirigindo com sentimento de solidariedade, cortesia e respeito 
+
+2- Dirigindo um veículo, o condutor que encontrar crianças, deficientes físicos atravessando a via deve:
+
+a) Diminuir a velocidade, buzinar e seguir em frente
+b) Para o veículo e facilitar a travessia
+c) Diminuir a velocidade, dar um sinal de luz e seguir em frente
+d) Diminuir a velocidade, desviar e seguir em frente
+
+3- Como deve proceder o condutor no caso de alagamento?
+
+a) Acelerar o carro
+b) Ficar parado até a chuva passar
+c) Ultrapassar a velocidade de 60 km/h
+d) Manter uma velocidade constante de 20 km/h para evitar a entrada de água no escapamento e no motor. 
+
+4- 0 condutor de veículo, transitando por vias onde haja escolas ou grande movimentação de pedestres:
+
+a) Conserva o veículo sempre na faixa esquerda da via 
+b) Transita em velocidade compatível com a segurança 
+c) Usa buzina para alertar os pedestres que estão passando 
+d) Acelera para deixar a via mais livre em menor tempo 
+
+5- Quem tem prioridade no trânsito, o pedestre ou o condutor?
+a) O pedestre, pois ele é mais fraco frente ao veículo
+b) O pedestre, desde que jovem ou idoso, pois são os mais desprotegidos 
+c) Como pessoas, ambos têm direitos iguais
+d) O condutor, pois ele está no comando do veículo
+
+6 - O trânsito sobre passeio e calçadas é permitido para:
+a) Fugir de congestionamento
+b) Apanhar passageiros
+c) Cortar caminho
+d) Acesso a estacionamento e/ou imóveis
+
+7- Assinale a alternativa correta. Frenagem de emergência é:
+a) Travamento das rodas
+b) Arrastamento dos pneus, sem deixar marcas no. solo 
+c) Redução moderada da velocidade do veículo
+d) Redução imediata da velocidade do veículo no menor tempo possível 
+
+8 - Em uma curva, a diminuição brusca da velocidade, provoca transferência de massa, fazendo com que o eixo dianteiro fique mais:
+a) Leve que o eixo traseiro, tendendo a "desgarrar a traseira" 
+b) Pesado que o eixo traseiro, tendendo a "desgarrar a traseira" 
+c) Leve que o eixo traseiro, tendendo a "desgarrar a dianteira" 
+d) Pesado que o eixo traseiro, tendendo a "desgarrar a dianteira" 
+
+9 - Nas rodovias, encontrando um trecho sobre nevoeiro intenso onde não há visibilidade, o condutor deve:
+
+a) Havendo necessidade estacionar na pista e sinalizar com o triângulo se segurança.
+b) Nunca parar.
+c) Aguardar no acostamento, até encontrar uma melhor condição de visibilidade. 
+d) Orientar-se pela faixa divisória da pista.
+
+10- Um veículo vem em sentido contrário ao seu, no período noturno com os faróis altos, provocando ofuscamento. Nessa situação você:
+a) Desvia o olhar para a direita e se orienta pelas faixas do solo. 
+b) Pára no acostamento até o outro veículo passar.
+c) Desvia o veículo para a esquerda e se orienta pelas faixas do solo. 
+d) Olha para o outro veículo e também, liga o farol alto. 
+
+11 - Chove forte e o limpador de pára-brisa de seu veículo não está sendo suficiente para manter a visibilidade. Nessa situação você:
+a) Pára o veículo em local seguro e aguarda.
+b) Abre as janelas e prossegue o trajeto.
+c) Acelera mais para dissipar os pingos de chuva.
+d) Liga os faróis altos.
+
+12 - As alterações exigidas nos limites de velocidade estabelecidos nas vias, variam de acordo com as condições:
+a) Máxima permitida para a via onde está circulando. : 
+b) Do veículo e da carga.
+c) Das condições meteorológicas.
+d) Todas as alternativas estão corretas.
+
+13 - O deslocamento do veículo de sua trajetória, devido à perda de aderência dos pneus com o solo, é chamada de:
+a) Derrapagem,
+b) Aquaplanagem.
+c) Hidroplanagem.
+d) frenagem.
+
+14-0 contato dos pneus com o solo é chamado de:
+a) Aderência.
+b) Derrapagem.
+c) Aquaplanagem.
+d) Hidroplanagem.
+
+15 - Para que o condutor tenha um comportamento seguro na via, é necessário:
+a) Ter conhecimento da Legislação e Regras de Circulação de trânsito. 
+b) Ser habilitado, no mínimo, há dois anos.
+c) Fazer revisão do seu veículo a cada dois meses.
+d) Ter dirigido, no mínimo, 150.000 km (cento e cinquenta mil quilômetros). 
+
+16-O fenômeno que ocorre quando os pneumáticos do veículo perdem o contato com o solo, pela presença de uma camada d'água entre a superfície da pista e os pneus é:
+a) Aquaplanagem.
+b) Condição adversa do tempo.
+c) Condição adversa da pista.
+d) Situação de derrapagem.
+17-Você está em uma via de bastante movimento e surge uma rotatória à frente com um veiculo lento fazendo seu contorno. Você deve:
+
+a) Buzinar para avisar que você está entrando com prioridade 
+
+b) Diminuir a velocidade e deixar o outro carro completar seu trajeto 
+c) Acelerar para adentrar à rotatória antes que aquele carro the atrapalhe 
+d) Estacionar o veículo até que o outro complete a travessia 
+
+18 - Quando é permitido o tráfego pelo acosta- mento de uma estrada?
+a) Quando a pista estiver congestionada
+b) Quando, devido a problema mecânico, o seu veículo estiver lento 
+c) Para acessar a lotes lindeiros ou fazer conversões 
+d) Para rebocar um veículo acidentado ou com problemas mecânicos 
+
+19 - Você está dirigindo em uma avenida, após uma chuva forte e, de repente, ocorre o fenômeno da aquaplanagem (o veiculo flutua sobre a lâmina d'água na pista), Nessa situação você:
+a) Tira o pé do acelerador e segura firmemente a direção até que seja restabelecida a aderência do pneu com o solo 
+b) Freia o veículo bruscamente para reestabelecer a aderência do pneu com o solo 
+c) Acelera o veiculo, segurando firmemente a direção
+d) Liga as luzes de emergência (pisca-alerta) para advertir aos demais motoristas da situação de risco 
+
+20 - Ao atravessar uma passagem de nivel com uma ferrovia, sem cancela, você deve:
+a) Reduzir a velocidade e atravessar a via férrea
+b) Parar o veículo, olhar para ambos os lados e
+efetuar o cruzamento com segurança
+c) Buzinar antes de atravessar
+d) Acender os faróis do veiculo
+
+21 - O procedimento correto do motociclista no meio de uma chuva 6:
+a) Acelerar gradativamente
+b) Reduzir a velocidade utilizando a embreagem
+c) Manter a aceleração constante não utilizando a embreagem 
+d) Frear bruscamente, para diminuir a velocidade
+
+22 - Os riscos e os perigos a que estamos sujeitos no trânsito estão relacionados com:
+a) Os veículos, condutores, as vias de trânsito, o
+ambiente e o comportamento das pessoas
+b) Os lotes lindeiros ao longo das vias
+c) As edificações nas áreas urbanas
+d) A sinalização das vias
+
+23 - É correto afirmar que, para evitar desgaste fisico durante o ato de dirigir, é necessário que o condutor:
+a) A posição do assento e do encosto do banco não
+influi no desgaste fisico do condutor
+b) Apoie bem o corpo no assento e no encosto do
+banco o mais próximo possivel de um ângulo de
+90 graus
+c) Apoie bem o corpo no assento, com as pernas,
+totalmente esticadas
+d) Apoie bem o encosto do banco o mais próximo
+possivel de um ângulo de 60 graus
+
+24-Dirigindo corretivamente, o condutor terá que:
+a) Corrigir uma situação de risco, não prevista
+e antecipada
+b) Diminuir a marcha nas lombadas.
+c) Dirigir com velocidade compatível com a
+segurança.
+d) Guardar distância de segurança do veículo
+que segue à frente.
+
+25 - O acidente que envolve, apenas um veículo, que não se conhecem suas possíveis causas:
+a) Colisão misteriosa.
+b) Colisão em uma passagem de nível (via
+férrea).
+c) Colisão com animais.
+d) Atropelamento.
+26 - Em uma freada brusca, arrastando os pneus do veículo você vai:
+a) Aumentar a distância de parada.
+b) Diminuir a distância de parada.
+c) Não há diferença.
+d) Depende do tipo de pavimento.
+27 - A distância percorrida pelo veículo, após o condutor perceber efetivamente o perigo, é denominada:
+a) Distância de parada.
+b) Distância de frenagem.
+c) Distância de percepção.
+d) Distância de reação.
+28 - Das condições abaixo citadas, aquelas que podem causar acidentes são as resultantes de:
+a) Adversidade do condutor
+b) Adversidade da documentação do veículo.
+c) Adversidade do socorrista
+d) Não se pode falar em adversidade, em casos
+de acidentes.
+29-Manter acesas as luzes de posição do veículo é obrigatório nos casos em que ocorre:
+a) Cerração, neblina, chuva forte.
+b) Somente quando ocorre cerração.
+c) Em qualquer situação.
+d) Ao trafegar em túneis com iluminação.
+
+30 - Constituem alguns equipamentos e sistemas do veículo importantes para evitar situações de perigo que podem levar a acidentes de trânsito:
+a) Suspensão e sistema de direção.
+b) Freios e pneus.
+c) Iluminação e lanternas indicadoras de
+direção.
+d) Todas as alternativas estão corretas
+`;
+
+exam = exam.trim();
+
+const matchTitle = exam.match(/(^[0-9])(.*)((\?|(:))$)/gim);
+const matchAnswersA = exam.match(/(^a\))(.*)/gim);
+const matchAnswersB = exam.match(/(^b\))(.*)/gim);
+const matchAnswersC = exam.match(/(^c\))(.*)/gim);
+const matchAnswersD = exam.match(/(^d\))(.*)/gim);
+
+const fullExam = [];
+
+const translateAnswer = {
+  a: 0,
+  b: 1,
+  c: 2,
+  d: 3,
+};
+
+const answers = [
+  "d",
+  "b",
+  "d",
+  "b",
+  "a",
+  "d",
+  "d",
+  "b",
+  "d",
+  "a",
+  "a",
+  "d",
+  "a",
+  "a",
+  "a",
+  "a",
+  "b",
+  "c",
+  "a",
+  "b",
+  "a",
+  "a",
+  "b",
+  "a",
+  "a",
+  "a",
+  "d",
+  "a",
+  "a",
+  "d",
+];
+
+console.log(matchTitle);
+
+for (let i = 0; i < 30; i++) {
+  matchTitle[i] = matchTitle[i].replace("-", "");
+  matchTitle[i] = matchTitle[i].replace(/^[0-9]+/, "");
+  matchAnswersA[i] = matchAnswersA[i].replace(/^a\)/gim, "");
+  matchAnswersB[i] = matchAnswersB[i].replace(/^b\)/gim, "");
+  matchAnswersC[i] = matchAnswersC[i].replace(/^c\)/gim, "");
+  matchAnswersD[i] = matchAnswersD[i].replace(/^d\)/gim, "");
+
+  const questionToInsert = {
+    question: matchTitle[i],
+    answers: [
+      matchAnswersA[i],
+      matchAnswersB[i],
+      matchAnswersC[i],
+      matchAnswersD[i],
+    ],
+    rightAnswer: translateAnswer[answers[i]],
+  };
+
+  fullExam.push(questionToInsert);
+}
+
+// require the fs module
+const fs = require("fs");
+
+// convert the array of objects to a string
+const str = JSON.stringify(fullExam);
+
+// write the string to a file
+fs.writeFile("data.json", str, function (err) {
+  if (err) throw err;
+  console.log("Data saved!");
+});
